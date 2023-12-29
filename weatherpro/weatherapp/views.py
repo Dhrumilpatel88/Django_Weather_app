2fcb7701c7498f2284836cecb9554eaff09c2be2
Dhrumil Patel <110415701+Dhrumilpatel88@users.noreply.github.com>
2023-12-29 20:05:19 +0530

    Initial commit

---
+++ weatherpro/weatherapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import urllib.request
+import json
+
+def index(request):
+
+    if request.method == 'POST':
+        city=request.POST['city']
+
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=metirc&appid=ef74559f4fe78e3b654bf27ca625b797').read()
+
+        list_of_data=json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °F',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        # print(data)
+    else:
+        data = {}
+    return render(request, "main/index.html", data)
